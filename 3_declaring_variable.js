055f8f1dc7f4264c0f64a9ec41a1567b13b7715e
Ga-Yeon-Baek <bgy6533@naver.com>
2025-09-09 21:51:47 +0900

    Create 3_declaring_variable.js

---
+++ 3_declaring_variable.js
@@ -0,0 +1,41 @@
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리'
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 할당하는 것.
+ * 2) 할당
+ */
+
+var name = '코드팩토리';
+console.log(name);
+
+let girlFriend;
+console.log(girlFriend);
+
+
+/*
+const 는 값을 항상 초기화 해주고 사용해야함.
+const girlFriend2 << 이렇게 사용하면 안됌.
+*/
