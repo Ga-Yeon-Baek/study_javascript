304e6343544920a49502f50352f224e532752d6f
Ga-Yeon-Baek <bgy6533@naver.com>
2025-09-09 21:59:46 +0900

    Create 14_object.js

---
+++ 14_object.js
@@ -0,0 +1,99 @@
+/**
+ * Object / 객체
+ */
+
+// key : value pair
+let yuJin = {
+    name : '안유진',
+    gruop : '아이브',
+    dance : function(){
+        return `${this.name}이 춤을 춥니다.`
+    }
+};
+// this : 현재 함수가 정의 되어있는 객체를 가르킨다.
+
+console.log(yuJin);
+console.log(yuJin.name);
+console.log(yuJin['name']);
+// 16, 17번은 같음.
+
+const key = 'name';
+
+console.log(yuJin[key]);
+
+console.log(yuJin.dance());
+
+
+const nameKey = 'name';
+const nameValue = '안유진';
+
+const groupKey = 'group';
+const groupValue = '아이브';
+
+const yuJin2 = {
+    [nameKey]: nameValue,
+    [groupKey]: groupValue,
+    dance : function(){
+        return`${this.name}이 춤을 춥니다.`;
+    }
+}
+console.log(yuJin2);
+console.log(yuJin2.dance());
+
+yuJin2['group'] = '코드팩토리';
+console.log(yuJin2);
+// 이런식으로 키값 밸류 변경 가능.
+
+yuJin2['englishName'] = 'An Yu Jin';
+console.log(yuJin2);
+// 이런식으로 위에서 정의 안해도 새롭게 키값 + 밸류 입력 가능.
+
+delete yuJin2['englishName'];
+console.log(yuJin2);
+// 삭제는 이렇게 delete로.
+
+
+/**
+ * const로 선언한 객체의 특징
+ * 
+ * 1) const로 선언할 경우 객체 자체를 변경 할 수는 없다.
+ * 2) 객체 안의 프로퍼티나 메서드는 변경 할 수 있다.
+ */
+
+const wonYoung = {
+    name : '장원영',
+    group : '아이브',
+}
+console.log(wonYoung);
+
+// wonYoung = {};
+// 이런식은 불가능.
+
+wonYoung['group'] = '코드팩토리';
+console.log(wonYoung);
+// 이런식은 가능.
+
+console.log('--------------');
+
+/**
+ * 모든 키 값 다 가져오기.
+ */
+
+console.log(Object.keys(wonYoung));
+
+/**
+ * 모든 벨류 값 다 가져오기
+ */
+
+console.log(Object.values(wonYoung));
+
+const name = '안유진';
+
+const yuJin3 = {
+    // name: name,
+    name,
+};
+console.log(yuJin3);
+
+//93번 라인이랑 94번 라인이랑 같다.
+// 네임의 키값과 변수의 이름이 똑같을때 생략 가능.
